5cb78c88f1b8812962b8b47b15406143f75206e1
juliana.reitz <juliana.mreitz@gmail.com>
2023-02-22 16:54:18 -0300

    feat - Car Service

--- src/Services/Car.service.ts
+++ src/Services/Car.service.ts
@@ -1,3 +1,32 @@
-// export default class CarService {
-//   private createDomain(car: Icar | null): Car | null
-// }
+import Car from '../Domains/Car.domain';
+import ICar from '../Interfaces/ICar';
+import CarODM from '../Models/CarODM';
+
+export default class CarService {
+  private registerCarDomain(car: ICar | null): Car | null {
+    if (car) {
+      return new Car(car);
+    }
+    return null;
+  }
+
+  public async register(car: ICar): Promise<Car | null> {
+    const carODM = new CarODM();
+    const newCar = await carODM.create(car);
+    return this.registerCarDomain(newCar);
+  }
+
+  public async getAll(): Promise<(Car | null) []> {
+    const carODM = new CarODM();
+    const getAll = await carODM.find();
+    const showAll = getAll.map((vehicle) => this.registerCarDomain(vehicle));
+    return showAll;
+  }
+
+  public async getById(id: string): Promise<Car | string | null> {
+    const carODM = new CarODM();
+    const getOne = await carODM.findById(id);
+    if (!getOne) return 'NOT_FOUND';
+    return this.registerCarDomain(getOne);
+  }
+}
