7e583ebb23034e00b99610f0be99abc2de10d97a
juliana.reitz <juliana.mreitz@gmail.com>
2023-02-22 15:58:25 -0300

    :car:[Juliana Reitz] CarShop

---
+++ tests/unit/Services/cars.test.ts
@@ -0,0 +1,14 @@
+// import { expect } from 'chai';
+// import sinon from 'sinon';
+// import { Model } from 'mongoose';
+// import { ICar } from '../../../src/Interfaces';
+// import CarService from '../../../src/Services/Car';
+
+// describe('At Car Service should', () => {
+//   it('01 - Register new car', async () => {
+//     sinon.stub(Model, 'create').resolves();
+//   });
+//   afterEach( () {
+//     sinon.restore();
+//   })
+// })
