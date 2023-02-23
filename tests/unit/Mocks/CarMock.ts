204f30d5aae1b21862a1b1def5489c47a701d348
juliana.reitz <juliana.mreitz@gmail.com>
2023-02-23 14:01:11 -0300

    feat - 30% Car Service tests

--- tests/unit/Mocks/CarMock.ts
+++ tests/unit/Mocks/CarMock.ts
@@ -35,6 +35,7 @@ export const updatedCar = {
 
 export const carsArray = [
   {
+    id: '6348513f34c397abcad040b2',
     model: 'Marea',
     year: 2002,
     color: 'Black',
@@ -44,6 +45,7 @@ export const carsArray = [
     seatsQty: 5,
   },
   {
+    id: '634852326b35b59438fbea31',
     model: 'Tempra',
     year: 1995,
     color: 'Black',
--- tests/unit/Services/CarsService.test.ts
+++ tests/unit/Services/CarsService.test.ts
@@ -4,7 +4,7 @@ import { Model } from 'mongoose';
 import CarService from '../../../src/Services/CarsService';
 import { validCar,
   validCarWithId,
-  carsArray,
+  // carsArray,
   validId,
 } from '../Mocks/CarMock';
 
@@ -13,22 +13,22 @@ describe('At Car Service should', function () {
   afterEach(function () { sinon.restore(); });
 
   it('01 - Register a new car', async function () {
-    sinon.stub(Model, 'create').resolves(validCar);
+    sinon.stub(Model, 'create').resolves(validCarWithId);
     const response = await carService.register(validCar);
     expect(response).to.be.deep.equal(validCarWithId);
   });
 
-  it('02 - Find all cars', async function () {
-    sinon.stub(Model, 'find').resolves(carsArray);
-    const response = await carService.getAll();
-    expect(response).to.be.deep.equal(carsArray);
-  });
-
-  it('03 - Find car by id', async function () {
-    sinon.stub(Model, 'findById').resolves(validCarWithId);
+  it('02 - Find car by id', async function () {
+    sinon.stub(Model, 'findOne').resolves(validCarWithId);
     const response = await carService.getById(validId);
     expect(response).to.be.deep.equal(validCarWithId);
   });
+
+  // it('02 - Find all cars', async function () {
+  //   sinon.stub(Model, 'find').resolves(carsArray);
+  //   const response = await carService.getAll();
+  //   expect(response).to.be.deep.equal(carsArray);
+  // });
 
   // it('04 - Find no car with wrong id', async function () {
   //   sinon.stub(Model, 'findById').resolves(InvalidIdMSG);
